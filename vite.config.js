2aed5c8e32635aef3ae290e8c648e6769cda01be
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2024-09-13 10:09:31 +0300

    Ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/weather-api/",
   plugins: [react()],
   server: {
     host: true, //now we can launch the app within mobile devices to test it
